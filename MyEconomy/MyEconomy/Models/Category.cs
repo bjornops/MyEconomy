1174b5e605fcfc417ff3cf824fc00d4fc0f1bf8f
bjornops <bjornops@gmail.com>
2018-07-19 18:30:44 +0200

    Added transaction and category models

---
+++ MyEconomy/MyEconomy/Models/Category.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+
+namespace MyEconomy.Models
+{
+    public class Category
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public List<Transaction> Transactions { get; } = new List<Transaction>();
+
+        public Category(string title, string description)
+        {
+            Title = title;
+            Description = description;
+        }
+
+        public void AddTransaction(Transaction transaction)
+        {
+            Transactions.Add(transaction);
+        }
+
+        public void RemoveTransaction(int index)
+        {
+            if(index >= 0 && index < Transactions.Count)
+            {
+                Transactions.RemoveAt(index);
+            }
+        }
+
+        public double Sum()
+        {
+            double sum = 0;
+            foreach(Transaction t in Transactions)
+            {
+                sum += t.Amount;
+            }
+            return sum;
+        }
+    }
+}
---
+++ MyEconomy/MyEconomy/Models/Transaction.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace MyEconomy.Models
+{
+    public class Transaction
+    {
+        public double Amount { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+
+        public Transaction(double amount, DateTime date, string title = "Amount", string description = "")
+        {
+            Amount = amount;
+            Title = title;
+            Description = description;
+            if (date != null)
+                Date = date;
+            else
+                Date = DateTime.UtcNow;
+        }
+    }
+}
