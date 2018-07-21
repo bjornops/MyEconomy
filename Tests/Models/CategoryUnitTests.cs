59c82a53f6ba7f2467e4660d5c51ef1a4707b423
bjornops <bjornops@gmail.com>
2018-07-21 15:38:15 +0200

    Added test project

---
+++ Tests/Models/CategoryUnitTests.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyEconomy.Models;
+
+namespace Tests
+{
+    [TestClass]
+    public class CategoryUnitTests
+    {
+        [TestMethod]
+        public void InstantiateCategory()
+        {
+            string title = "Title";
+            string description = "Description";
+            Category category = new Category(title, description);
+
+            Assert.AreEqual(title, category.Title);
+        }
+    }
+}
---
+++ Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("6dede377-f703-419a-ba78-f1b8ee853361")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
