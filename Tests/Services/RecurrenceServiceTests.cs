fab33e0b621d2d2ceb78c0949afcf05266c24839
bjornops <bjornops@gmail.com>
2018-07-22 10:15:46 +0200

    Added reccurrence service tests

---
+++ Tests/Services/RecurrenceServiceTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyEconomy.Services;
+
+namespace Tests.Services
+{
+    [TestClass]
+    public class RecurrenceServiceTests
+    {
+        [TestMethod]
+        public void GetSubsequentDateSingleMonth()
+        {
+            DateTime initialDate = new DateTime(2018, 01, 01);
+            RecurrenceService.IntervalLength interval = RecurrenceService.IntervalLength.Month;
+            DateTime subsequentDate = RecurrenceService.GetSubsequentDate(initialDate, interval);
+
+            DateTime expectedDate = new DateTime(2018, 02, 01);
+            Assert.AreEqual(expectedDate, subsequentDate);
+        }
+
+        [TestMethod]
+        public void GetSubsequentDateTwoSubsequentMonths()
+        {
+            DateTime initialDate = new DateTime(2018, 01, 31);
+            RecurrenceService.IntervalLength interval = RecurrenceService.IntervalLength.Month;
+            DateTime firstSubsequentDate = RecurrenceService.GetSubsequentDate(initialDate, interval);
+            DateTime secondSubsequentDate = RecurrenceService.GetSubsequentDate(firstSubsequentDate, interval);
+
+            DateTime expectedDate = new DateTime(2018, 03, 31);
+            Assert.AreEqual(expectedDate, secondSubsequentDate);
+        }
+    }
+}
