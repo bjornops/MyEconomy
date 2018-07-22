564dfb0cffb0c3fb77810980f05a0db942fce54b
bjornops <bjornops@gmail.com>
2018-07-22 12:51:56 +0200

    MockDataService yields date sorted transactions

--- MyEconomy/MyEconomy/Services/DataServiceMock.cs
+++ MyEconomy/MyEconomy/Services/DataServiceMock.cs
@@ -27,10 +27,17 @@ namespace MyEconomy.Services
         {
             int numberOfTransactions = 4 * categories.Count;
 
+            List<Transaction> transactions = new List<Transaction>();
             for (int i = 0; i < numberOfTransactions; i++)
             {
-                Transaction transaction = GenerateTransaction();
-                categories[_random.Next(0, categories.Count)].AddTransaction(transaction);
+                transactions.Add(GenerateTransaction());
+            }
+
+            transactions.Sort((x, y) => x.Date.CompareTo(y.Date));
+
+            for (int i = 0; i < numberOfTransactions; i++)
+            {
+                categories[_random.Next(0, categories.Count - 1)].AddTransaction(transactions[i]);
             }
 
             return categories;
