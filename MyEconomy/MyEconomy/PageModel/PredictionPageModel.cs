f6a2a63c47c5ecad5fc20d5ef038ea4edde3b0f5
bjornops <bjornops@gmail.com>
2018-07-25 12:37:30 +0200

    Added balance prediction to PredictionPage

--- MyEconomy/MyEconomy/PageModel/PredictionPageModel.cs
+++ MyEconomy/MyEconomy/PageModel/PredictionPageModel.cs
@@ -26,7 +26,9 @@ namespace MyEconomy.PageModels
 
         public override void Init(object initData)
         {
-            ApplyDefaultPlotModel();
+            //ApplyDefaultPlotModel();
+            PredictionPlotModel = GenerateBalancePlotModel(_dataService.GetCategories(), 10000);
+            var x = 1;
         }
 
         // Methods are automatically wired up to page
@@ -87,6 +89,139 @@ namespace MyEconomy.PageModels
             plotModel.Series.Add(GenerateLineSeries(category));
 
             return plotModel;
+        }
+
+        private PlotModel GenerateBalancePlotModel(List<Category> categories, double currentBalance)
+        {
+            PlotModel plotModel = new PlotModel { Title = "Predicted balance" };
+
+            var minValue = DateTimeAxis.ToDouble(DateTime.UtcNow);
+            var maxValue = DateTimeAxis.ToDouble(DateTime.UtcNow.AddDays(90));
+
+            plotModel.Axes.Add(new DateTimeAxis { Position = AxisPosition.Bottom, Minimum = minValue, Maximum = maxValue, StringFormat = "MMM" });
+
+            plotModel.Series.Add(GenerateBalanceLineSeries(categories, currentBalance));
+
+            return plotModel;
+            
+        }
+
+        private LineSeries GenerateBalanceLineSeries(List<Category> categories, double currentBalance)
+        {
+            LineSeries lineSeries = new LineSeries();
+
+            List<Transaction> mergedCategoryTransactions = MergeCategories(categories);
+
+            List<Transaction> transactionList = FilterTransactionPeriod(mergedCategoryTransactions, DateTime.UtcNow, new DateTime(2100, 01, 01));
+            transactionList.Sort((x, y) => x.Date.CompareTo(y.Date));
+
+            List<DataPoint> dateSummedTransactions = GetDateSummedDataPoints(transactionList);
+
+            List<DataPoint> accumulatedDailySum = AccumulateDataPoints(dateSummedTransactions, currentBalance);
+
+            foreach (DataPoint dataPoint in accumulatedDailySum)
+            {
+                lineSeries.Points.Add(dataPoint);
+            }
+
+            return lineSeries;
+        }
+
+        private List<Transaction> FilterTransactionPeriod(List<Transaction> transactions, DateTime fromDate, DateTime toDate)
+        {
+            List<Transaction> filteredTransactions = new List<Transaction>();
+
+            foreach(Transaction transaction in transactions)
+            {
+                if (transaction.Date.Date >= fromDate.Date && transaction.Date.Date <= toDate.Date)
+                    filteredTransactions.Add(transaction);
+            }
+            return filteredTransactions;
+        }
+
+        private List<Transaction> MergeCategories(List<Category> categories)
+        {
+            List<Transaction> mergedCategoryTransactions = new List<Transaction>();
+
+            foreach (Category category in categories)
+            {
+                mergedCategoryTransactions.AddRange(category.Transactions);
+            }
+
+            return mergedCategoryTransactions;
+        }
+
+        private bool IsSameDate(DateTime d1, DateTime d2)
+        {
+            return (d1.Date == d2.Date);
+        }
+
+        private List<DataPoint> AccumulateDataPoints(List<DataPoint> dataPoints, double initialValue = 0)
+        {
+            List<DataPoint> accumulatedDataPoints = new List<DataPoint>();
+            double accumulatedValue = initialValue;
+            
+            foreach(DataPoint dataPoint in dataPoints)
+            {
+                accumulatedValue += dataPoint.Y;
+                accumulatedDataPoints.Add(new DataPoint(dataPoint.X, accumulatedValue));
+            }
+
+            return accumulatedDataPoints;
+        }
+
+        private List<DataPoint> GetAccumulatedGroupedDataPoints(List<Transaction> transactions, double currentBalance)
+        {
+            transactions.Sort((x, y) => x.Date.CompareTo(y.Date));
+            List<DataPoint> dailyGroupedDataPoints = GetDateSummedDataPoints(transactions);
+
+            double dailySum = 0;
+            DateTime dateIterator = transactions[0].Date;
+
+            foreach (Transaction transaction in transactions)
+            {
+                if (transaction.Date == dateIterator)
+                    dailySum += transaction.Amount;
+                else
+                {
+                    dailyGroupedDataPoints.Add(new DataPoint(DateTimeAxis.ToDouble(dateIterator), dailySum));
+                    dateIterator = transaction.Date;
+                    dailySum += transaction.Amount;
+                }
+                if (transaction == transactions[transactions.Count - 1])
+                {
+                    dailySum += transaction.Amount;
+                    dailyGroupedDataPoints.Add(new DataPoint(DateTimeAxis.ToDouble(dateIterator), dailySum));
+                }
+            }
+
+            return dailyGroupedDataPoints;
+        }
+
+        private List<DataPoint> GetDateSummedDataPoints(List<Transaction> transactions)
+        {
+            transactions.Sort((x, y) => x.Date.CompareTo(y.Date));
+            List<DataPoint> dailyGroupedDataPoints = new List<DataPoint>();
+
+            double dailySum = 0;
+            DateTime dateIterator = transactions[0].Date;
+
+            foreach (Transaction transaction in transactions)
+            {
+                if (IsSameDate(transaction.Date, dateIterator))
+                    dailySum += transaction.Amount;
+                else
+                {
+                    dailyGroupedDataPoints.Add(new DataPoint(DateTimeAxis.ToDouble(dateIterator), dailySum));
+                    dateIterator = transaction.Date;
+                    dailySum = transaction.Amount;
+                }
+                if(transaction == transactions[transactions.Count - 1])
+                    dailyGroupedDataPoints.Add(new DataPoint(DateTimeAxis.ToDouble(dateIterator), dailySum));
+
+            }
+
+            return dailyGroupedDataPoints;
         }
 
         private LineSeries GenerateLineSeries(Category category)
